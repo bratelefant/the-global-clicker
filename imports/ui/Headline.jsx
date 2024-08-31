1b28e88659fddaf1d055eb768fce341cf3c056d1
bratelefant <59534030+bratelefant@users.noreply.github.com>
2024-08-31 12:49:04 +0200

    bugfix missing headline

--- imports/ui/Headline.jsx
+++ imports/ui/Headline.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 const styles = { headline: { color: "white" } };
 
-export const Headline = ({ children }) => {
-  <h1 style={styles.headline}>{children}</h1>;
-};
+export const Headline = ({ children }) => (
+  <h1 style={styles.headline}>{children}</h1>
+);
